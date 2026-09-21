8a8c6e11c10d998b4663274b73c48f162a1bd457
MiniJohan <johanmdelin@outlook.com>
2026-09-21 21:57:33 +0200

    Fps logging

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
 
 print("Camera opened:", cap.isOpened())
 
+frame_count = 0
+start_time = time.time()
+
 while True:
     ret, frame = cap.read()
-    print(frame.shape)
-    print(frame.dtype)
-    break
 
     if not ret:
         print("Failed to grab frame.")
         break
+
+    frame_count += 1
+    current_time = time.time()
+    elapsed = current_time - start_time
+
+    if elapsed >= 1:
+        fps = frame_count / elapsed
+
+        print("FPS:", fps)
+
+        frame_count = 0
+        start_time = current_time
 
     cv2.imshow("CamOS", frame)
 
